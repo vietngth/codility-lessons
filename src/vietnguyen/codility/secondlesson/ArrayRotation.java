257b316b59db83c8ea5b5639717c80f22ecd38e4
vietnguyenthe97 <vietnguyenthe97@gmail.com>
2020-06-20 20:24:47 +0700

    Array Rotation: More Optimized Solution

--- src/vietnguyen/codility/secondlesson/ArrayRotation.java
+++ src/vietnguyen/codility/secondlesson/ArrayRotation.java
@@ -1,41 +1,28 @@
 package vietnguyen.codility.secondlesson;
 
 public class ArrayRotation {
-    public static int[] rotation(int arr[]) {
-        int size = arr.length;
-        int[] copyArr = new int[size];
-        int lastValue = arr[size-1];
-        int j=0;
+	public int[] solution(int[] A, int K) {
+		int size = A.length;
+		// any size = K just rotates as the same position
+		if (size == K) {
+			return A;
+		}
 
-        for (int i=1; i<size; i++) {
-            copyArr[i] = arr[j++];
-        }
-        copyArr[0] = lastValue;
-        return copyArr;
-    }
+		// any size = K would make the array the same => just minus redundant rotations
+		while (K > size) {
+			K = K - size;
+		}
 
-    public static int[] rotations(int arr[], int rotations) {
-        int size = arr.length;
-        if (size == rotations) {
-            return arr;
-        }
+		int[] B = new int[size];
+		int j = 0;
+		for (int i = K; i < size; i++) {
+			B[i] = A[j++];
+		}
 
-        for (int i=0; i< rotations; i++) {
-            arr = rotation(arr);
-        }
-        return arr;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        int size = 5;
-        int[] newArr = rotations(arr, 3);
-        for(int i=0; i<size; i++) {
-            System.out.print(newArr[i] + " ");
-        }
-
-        System.out.println();
-        int a=3, b=5;
-        System.out.println(a ^ b);
-    }
+		for (int i = 0; i < K; i++) {
+			B[i] = A[j++];
+		}
+		return B;
+	}
 }
+
